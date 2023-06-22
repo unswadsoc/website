efb08e2750d7013adb7f31cb10df34c81e8287f0
adriantan09 <adrian.tan09@gmail.com>
2023-06-22 23:26:07 +1000

    dropdown menu hotfix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -33,7 +33,7 @@ export default async function Navigation() {
           })}
         </ul>
       </div>
-      <div className="dropdown navbar-end flex lg:hidden w-2/3">
+      <div className="dropdown navbar-end flex lg:hidden w-2/3 z-[1] ">
         <label tabIndex={0} className="btn btn-ghost lg:hidden text-white">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </label>
