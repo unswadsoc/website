c34d0e95f2ebd0ceb8a612e57a34c1882b4d981a
RichardZYang <richardyang2468@gmail.com>
2025-04-16 18:41:33 +1000

    added refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 60;
+
 import { createClient } from '../../prismicio';
 import { getEvents } from '../lib/api';
 import { SliceZone } from '@prismicio/react';
@@ -7,15 +9,13 @@ export default async function Home() {
   const client = createClient();
   const page = await client.getByUID('page', 'home');
 
-  const events = await getEvents()
+  const events = await getEvents();
 
   return (
-    <>
-      <SliceZone
-        slices={page.data.slices}
-        components={components}
-        context={{ events }}
-      />
-    </>
-  )
+    <SliceZone
+      slices={page.data.slices}
+      components={components}
+      context={{ events }}
+    />
+  );
 }
